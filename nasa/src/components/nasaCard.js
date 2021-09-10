6020abd412ee4c2e492751d5d8e41d5a3160debd
Aaron Williams <aaronrw4@yahoo.com>
2021-09-10 13:14:18 -0700

    Added HTML to the page

--- nasa/src/components/nasaCard.js
+++ nasa/src/components/nasaCard.js
@@ -4,13 +4,24 @@ import { nasaContext } from '../context/nasaContext';
 const NasaCard = (props) => {
     let photoData = useContext(nasaContext)
     console.log(photoData)
-    
     return (
         <div>
             {!(props.date in photoData) ? 
                 <div>Loading</div>
                 :
-                <img src={photoData[props.date]['url']}/>
+                <div>
+                    <header>
+                        <h1 className='title'>{photoData[props.date]['title']}</h1>
+                        <p className='date'>{props.date}</p>
+                        <a href='https://www.nasa.gov/'>
+                            <img className='symbol' src='https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg'/>
+                        </a>
+                    </header>
+                    <a href={photoData[props.date]['hdurl']} target="_blank">
+                        <img className='image' src={photoData[props.date]['url']}/>
+                    </a>
+                    <p className='explanation'>{photoData[props.date]['explanation']}</p>
+                </div>
             }
         </div>
     )
