cd3f9ac6a5becf65baa983a3e246002e0628f6ff
Aaron Williams <aaronrw4@yahoo.com>
2021-09-22 18:34:36 -0700

    Added react-player with condition statement

--- nasa/src/components/nasaCard.js
+++ nasa/src/components/nasaCard.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as solid} from '@fortawesome/free-solid-svg-icons';
 import { faHeart as regular} from '@fortawesome/free-regular-svg-icons';
 import { DateBotton } from './dateButton';
+import ReactPlayer from 'react-player';
 
 const NasaCard = (props) => {
     let photoData = useContext(nasaContext)
@@ -21,9 +22,18 @@ const NasaCard = (props) => {
                             <img className='symbol' src='https://www.nasa.gov/sites/all/themes/custom/nasatwo/images/nasa-logo.svg'/>
                         </a>
                     </header>
-                    <a href={photoData[props.date]['hdurl']} target="_blank">
-                        <img className='image' src={photoData[props.date]['url']}/>
-                    </a>
+                    {photoData[props.date]['media_type'] === 'video' ? 
+                        <ReactPlayer
+                            url={photoData[props.date]['url']}
+                            playing
+                            loop
+                            height='60vh'
+                            className='video'
+                        /> :
+                        <a href={photoData[props.date]['hdurl']} target="_blank">
+                            <img className='image' src={photoData[props.date]['url']}/>
+                        </a>
+                    }
                     <button onClick={props.handleClick} className='likeButton'>
                         {photoData[props.date]['like'] === false ? 
                             <FontAwesomeIcon icon={regular} size="3x" color='white'/> : 
